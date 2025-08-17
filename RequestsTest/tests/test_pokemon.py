4bd335020309ce76a25dee681d2a45a86e8aa2cc
Sergey <ruxaker1@gmail.com>
2025-08-17 21:52:23 +0400

    Update test_pokemon.py

--- RequestsTest/tests/test_pokemon.py
+++ RequestsTest/tests/test_pokemon.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = 'de4828625696701ae787c1eb2aa7cbd9'
+TOKEN = 'USER_TOKEN'
 HEADER = {'Content-Type' : 'application/json', 'trainer_token': TOKEN}
 TRAINER_ID = '39134'
 
@@ -12,4 +12,5 @@ def test_status_code_trainer():
 
 def test_name_trainer():
     response_get = requests.get(url = f'{URL}/me/', headers = HEADER)
+
     assert response_get.json()['data'][0]['trainer_name'] == 'Баунти'
